0995f9a83897a550f0a3f2ba5f55272cb7c24f8b
Raju Kumar <raju.kumar@scaleworx.io>
2024-08-30 10:50:26 +0530

    worked on branch

--- src/routes/authRoute.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-import UserController from '../controller/authController';
-import bodyParser from 'body-parser';
-const userController= new UserController()
-
-export const authroute= express.Router()
-const jsonparser=bodyParser.json()
-authroute.post('/signup',jsonparser,userController.Signup)
-authroute.post('/login',jsonparser,userController.UserLogin)
